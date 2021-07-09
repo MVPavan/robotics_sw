c43107cdd6ea0f544e953499b7a630779e6a51b6
PavanMV <pavanmv1993531@gmail.com>
2021-07-09 12:18:46 +0530

    converted  ball distances to velocites

--- Projects/GoChaseIt/catkin_ws/src/target_chaser/src/target_locator.cpp
+++ Projects/GoChaseIt/catkin_ws/src/target_chaser/src/target_locator.cpp
@@ -16,11 +16,25 @@ public:
     TargetLocator(int argc, char** argv);
     ~TargetLocator() = default;
 
-    void locate_white_ball(const sensor_msgs::Image);
+    void locate_white_ball(const sensor_msgs::Image& img);
+    void feed_velocities(float linear_x, float angular_z);
 };
 
-void TargetLocator::locate_white_ball(const sensor_msgs::Image img) {
-    int x, y, count, x_target {0};
+void TargetLocator::feed_velocities(float linear_x, float angular_z){
+    ROS_INFO_STREAM("Chasing the target .....");
+    target_chaser::ChaseTarget srv;
+    srv.request.linear_x = linear_x;
+    srv.request.angular_z = angular_z;
+
+    if(!client.call(srv))
+        ROS_ERROR("Sorry boss! failed to chase target!!");
+}
+
+void TargetLocator::locate_white_ball(const sensor_msgs::Image& img) {
+    int x{0}, y{0}, bin_size{80}, azimuth_shift{0}, range{0};
+    double count{0}, max_ball_image_size{double(3.14*200*200)};
+    float  x_factor{0.01}, z_factor{0.1}, linear_x{0.0}, angular_z{0.0};
+    bool chase_it_flag{false};
     for(size_t i{0}; i<img.data.size(); i+=3){
         if(
                 img.data.at(i)==255 &&
@@ -33,10 +47,26 @@ void TargetLocator::locate_white_ball(const sensor_msgs::Image img) {
             count++;
         }
     }
-    if(count==0)
-        x_target=0;
-    else
-        x_target = x/count - img.width/2;
+    if (count < max_ball_image_size) {
+        if (count == 0){
+            azimuth_shift = 0;
+            ROS_INFO_STREAM("Target Missing ....!!!");
+        }
+        else{
+            azimuth_shift = img.width / 2 - x / count;
+            angular_z = azimuth_shift*z_factor/bin_size;
+            linear_x = max_ball_image_size*x_factor/count;
+            feed_velocities(linear_x=linear_x,angular_z=angular_z);
+            chase_it_flag= true;
+        }
+    }
+    else{
+        ROS_INFO_STREAM("Target in custody !!!");
+    }
+    if(!chase_it_flag){
+        feed_velocities(linear_x=linear_x,angular_z=angular_z);
+    }
+
 }
 
 TargetLocator::TargetLocator(int argc, char** argv){
