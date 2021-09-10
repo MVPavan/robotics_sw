00333e53088fdb6718b1d5a98019998ab3a75cbe
pavanmv <pavanmv1993531@gmail.com>
2021-09-10 09:06:56 +0530

    working test_navigation

---
+++ Projects/HomeServiceRobot/catkin_ws/src/scripts/test_navigation.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+catkin_ws_dir="$( cd "$script_dir/../.." && pwd )"
+
+cd "$catkin_ws_dir" || exit
+
+xterm -e " source devel/setup.sh; roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 5
+#xterm -e " source devel/setup.sh; roslaunch turtlebot_gazebo gmapping_demo.launch " &
+#sleep 2
+xterm -e " source devel/setup.sh; roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 2
+xterm -e " source devel/setup.sh; roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 2
+#xterm -e " source devel/setup.sh; roslaunch turtlebot_teleop keyboard_teleop.launch " &
+#sleep 2
+#xterm -e " source devel/setup.sh; rosrun rqt_image_view rqt_image_view "
+
